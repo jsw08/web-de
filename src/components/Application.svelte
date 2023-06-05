d1bab42b76c605dad5952abaff99ecd7886919b3
jsw08 <jurnwubben@gmail.com>
2023-06-05 10:04:44 +0200

    init + working on application

---
+++ src/components/Application.svelte
@@ -0,0 +1,8 @@
+<script type="ts">
+
+</script>
+
+<div class="absolute left-[15px] top-[15px] min-w-52 min-h-52 resize overflow-auto p-4 rounded-xl shadow-xl border-2">
+    hi
+
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+// @ts-ignore
 import { defineConfig } from 'vite'
+// @ts-ignore
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+// @ts-ignore
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), cssInjectedByJsPlugin()],
 })
